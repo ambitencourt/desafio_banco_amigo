682dc58611939d05ca2bb6b1f5d532c8b5407993
ambitencourt <adriano.mirandabitencourt@gmail.com>
2022-12-04 23:56:07 -0300

    Correction

--- lib/app/modules/bank/data/infra/models/account_model.dart
+++
@@ -1,8 +0,0 @@
-import 'package:banco_amigo/app/modules/bank/domain/entities/account_entity.dart';
-
-class AccountModel extends AccountEntity {
-  AccountModel(
-      {required super.bankNumber,
-      required super.agencyNumber,
-      required super.accountNumber});
-}
